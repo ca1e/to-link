2da27e8c4680c892b2903bf3b1449b0beb087cd6
NPC-C83H <i@scp500.com>
2019-12-07 21:48:13 +0800

    Update main.go
    CORS

--- src/main.go
+++ src/main.go
@@ -111,11 +111,26 @@ func redirect(c *gin.Context){
     }
 }
 
+func CORSMiddleware(c *gin.Context){
+       c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+}
+
 func main() {
     runtime.GOMAXPROCS(runtime.NumCPU());
     // gin.SetMode(gin.ReleaseMode)
 
     r := gin.Default()
+    r.Use(CORSMiddleware)
 
     // r.Static("/static", "../html/static")
     r.StaticFile("/", "../html/index.html")
